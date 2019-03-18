482a8e82fb342958afeb556c15d53c5964876926
Irvin Belloni <irvin.belloni@ticketmaster.com>
2019-03-18 12:05:29 -0400

    Removed unused exception

--- qoq-rest-core/src/main/java/com/web2h/qoq/rest/core/exception/not/found/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.web2h.qoq.rest.core.exception.not.found;
-
-public class NotFoundException extends Exception {
-
-	private static final long serialVersionUID = 1L;
-
-}
--- qoq-rest-core/src/main/java/com/web2h/qoq/rest/core/exception/not/found/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.web2h.qoq.rest.core.exception.not.found;
-
-public class UserNotFoundException extends NotFoundException {
-
-	private static final long serialVersionUID = 1L;
-
-}
